36b4e6ea23b606b11c7feea3e008d23a28e27e8a
Asier <1537266+asierba@users.noreply.github.com>
2017-06-21 20:06:42 +0100

    Add about page and simple routing

---
+++ src/components/aboutPage.js
@@ -0,0 +1,13 @@
+const React = require('react');
+
+const AboutPage = React.createClass({
+    render: () => {
+        return (
+            <div>
+            about page
+            </div>
+        );
+    }
+});
+
+module.exports = AboutPage;
--- src/main.js
+++ src/main.js
@@ -1,4 +1,31 @@
 $ = jQuery = require('jquery');
 const React = require('react');
 const HomePage = require('./components/homePage');
-React.render(<HomePage/>, document.getElementById("app"));
+const AboutPage = require('./components/aboutPage');
+
+const App = React.createClass({
+    render: function() {
+        let Page;
+        // if (this.props.route == 'about') {
+        //     Page = AboutPage;
+        // } else {
+        //     Page = HomePage;
+        // }
+        switch(this.props.route) {
+            case 'about': Page = AboutPage; break;
+            default: Page = HomePage; break;
+        }
+        return (
+            <div>
+                <Page></Page>
+            </div>
+        );
+    }
+});
+
+function render() {
+    const route = window.location.hash.substr(1);
+    React.render(<App route={route}/>, document.getElementById("app"));
+}
+window.onhashchange = render;
+render();
