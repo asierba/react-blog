a49a958426c80394a6c01414a9ed8ad859b4e663
Asier <asierba@gmail.com>
2017-06-30 18:18:46 +0100

    use react Link in the header

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,14 @@
 const React = require('react');
+const Router = require('react-router');
+const Link = Router.Link;
 
 const Header = React.createClass({
     render: () => {
         return (
             <ul className="nav nav-tabs">
-                <li><a href="/">Home</a></li>
-                <li><a href="#posts">Posts</a></li>
-                <li><a href="#about">About</a></li>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="posts">Posts</Link></li>
+                <li><Link to="about">About</Link></li>
             </ul>
         )
     }
