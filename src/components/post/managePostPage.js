f6574fa6b138ae750df33f1005317fe8ab27a836
Asier <1537266+asierba@users.noreply.github.com>
2017-07-01 14:00:19 +0100

    add manage post page

---
+++ src/components/post/managePostPage.js
@@ -0,0 +1,11 @@
+const React = require('react');
+
+const ManagePostPage = React.createClass({
+    render: function () {
+        return (
+            <h1>Manage Post</h1>
+        );
+    }
+});
+
+module.exports = ManagePostPage;
