3eae8a5eca041641d9304832a5a1133bdfa8751d
Asier <asierba@gmail.com>
2017-07-02 14:29:55 +0100

    add two way binding in form

--- src/components/post/managePostPage.js
+++ src/components/post/managePostPage.js
@@ -2,10 +2,19 @@ const React = require('react');
 const PostForm = require('./postForm');
 
 const ManagePostPage = React.createClass({
+    getInitialState: () => {
+        return { title: '', content: ''} ;
+    },
     render: function () {
         return (
-            <PostForm/>
+            <PostForm post={this.state} onChange={this.onChange}/>
         );
+    },
+    onChange : function(event) {
+        const value = event.target.value;
+        const field = event.target.name;
+        this.state[field] = value;
+        this.setState(this.state);
     }
 });
 
