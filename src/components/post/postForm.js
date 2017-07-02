c43d36b281c21a509c79eecba182d1e7b108f55f
Asier <asierba@gmail.com>
2017-07-02 23:09:08 +0100

    Preview of the post is shown while tipying in the form

--- src/components/post/postForm.js
+++ src/components/post/postForm.js
@@ -1,29 +1,37 @@
 const React = require('react');
+const marked = require('marked');
 
 const PostForm = React.createClass({
     render: function () {
         return (
-            <form>
-                <h1>Manage Post</h1>
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-md-6">
+                        <form>
+                            <h1>Add Post</h1>
 
-                <label htmlFor="title">Title</label>
-                <input type="text" name="title" className="form-control" placeholder="title"
-                       value={this.props.post.title}
-                       onChange={this.props.onChange}/>
-                <br/>
+                            <label htmlFor="title">Title</label>
+                            <input type="text" name="title" className="form-control" placeholder="title"
+                                   value={this.props.post.title}
+                                   onChange={this.props.onChange}/>
+                            <br/>
 
-                <label htmlFor="content">Content</label>
-                <textarea name="content" className="form-control" rows="20" placeholder="content"
-                          value={this.props.post.content}
-                          onChange={this.props.onChange}
-                ></textarea>
-                <br/>
+                            <label htmlFor="content">Content</label>
+                            <textarea name="content" className="form-control" rows="20" placeholder="content"
+                                      value={this.props.post.content}
+                                      onChange={this.props.onChange}
+                            ></textarea>
+                            <br/>
 
-                <div> {this.props.post.title} </div>
-                <div> {this.props.post.content} </div>
-
-                <input type="submit" value="Save" className="btn btn-default"/>
-            </form>
+                            <input type="submit" value="Save" className="btn btn-default"/>
+                        </form>
+                    </div>
+                    <div className="col-md-6">
+                        <h1> {this.props.post.title} </h1>
+                        <div dangerouslySetInnerHTML={{__html: marked(this.props.post.content)}}></div>
+                    </div>
+                </div>
+            </div>
         );
     }
 });
