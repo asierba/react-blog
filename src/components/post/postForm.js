53c2ccf865782fea0d59fe7ae136b1f8d1060930
Asier <1537266+asierba@users.noreply.github.com>
2017-07-02 14:29:55 +0100

    add two way binding in form

--- src/components/post/postForm.js
+++ src/components/post/postForm.js
@@ -7,12 +7,20 @@ const PostForm = React.createClass({
                 <h1>Manage Post</h1>
 
                 <label htmlFor="title">Title</label>
-                <input type="text" name="title" className="form-control" placeholder="title" />
+                <input type="text" name="title" className="form-control" placeholder="title"
+                       value={this.props.post.title}
+                       onChange={this.props.onChange}/>
                 <br/>
 
                 <label htmlFor="content">Content</label>
-                <textarea name="content" className="form-control" rows="20" placeholder="content"></textarea>
+                <textarea name="content" className="form-control" rows="20" placeholder="content"
+                          value={this.props.post.content}
+                          onChange={this.props.onChange}
+                ></textarea>
                 <br/>
+
+                <div> {this.props.post.title} </div>
+                <div> {this.props.post.content} </div>
 
                 <input type="submit" value="Save" className="btn btn-default"/>
             </form>
