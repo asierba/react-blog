0ff8a6eb15536c513015de51f85c894b01c4b121
Asier <1537266+asierba@users.noreply.github.com>
2017-07-03 19:05:25 +0100

    basic validation in add post form

--- src/components/post/postForm.js
+++ src/components/post/postForm.js
@@ -3,6 +3,13 @@ const marked = require('marked');
 
 const PostForm = React.createClass({
     render: function () {
+        function getFromGroupClasses(errors) {
+            return errors ?
+                'form-group has-error' :
+                "form-group";
+
+        }
+
         return (
             <div className="container-fluid">
                 <div className="row">
@@ -10,18 +17,21 @@ const PostForm = React.createClass({
                         <form>
                             <h1>Add Post</h1>
 
-                            <label htmlFor="title">Title</label>
-                            <input type="text" name="title" className="form-control" placeholder="title"
-                                   value={this.props.post.title}
-                                   onChange={this.props.onChange}/>
-                            <br/>
+                            <div className={getFromGroupClasses(this.props.post.errors.title)}>
+                                <label htmlFor="title">Title</label>
+                                <input type="text" name="title" className="form-control" placeholder="title"
+                                       value={this.props.post.title}
+                                       onChange={this.props.onChange}/>
+                                <div>{this.props.post.errors.title}</div>
+                            </div>
 
-                            <label htmlFor="content">Content</label>
-                            <textarea name="content" className="form-control" rows="20" placeholder="content"
-                                      value={this.props.post.content}
-                                      onChange={this.props.onChange}
-                            ></textarea>
-                            <br/>
+                            <div className={getFromGroupClasses(this.props.post.errors.content)}>
+                                <label htmlFor="content">Content</label>
+                                <textarea name="content" className="form-control" rows="20" placeholder="content"
+                                          value={this.props.post.content}
+                                          onChange={this.props.onChange}></textarea>
+                                <div>{this.props.post.errors.content}</div>
+                            </div>
 
                             <input type="submit" value="Save" className="btn btn-default" onClick={this.props.onSave} />
                         </form>
