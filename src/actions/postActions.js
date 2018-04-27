95b8eba7817e9de9630d3f80205c41b10bae6c1f
Asier <asierba@gmail.com>
2018-04-27 13:05:20 +0100

    data is fetched through a local api run by express

--- src/actions/postActions.js
+++ src/actions/postActions.js
@@ -3,17 +3,19 @@ const PostApi = require('../api/postApi');
 
 const PostActions = {
     initialisePosts : () => {
-      const posts = PostApi.getAll();
-      Dispatcher.dispatch({
-         actionType: 'POSTS_LOADED',
-          posts: posts
-      });
+      PostApi.getAll().then(posts => {
+          Dispatcher.dispatch({
+             actionType: 'POSTS_LOADED',
+              posts: posts
+          });
+        });
     },
     createPost: (post) => {
-        const newPost = PostApi.savePost(post);
-        Dispatcher.dispatch({
-            actionType: 'POST_CREATED',
-            post: newPost
+        PostApi.savePost(post).then(newPost => {
+            Dispatcher.dispatch({
+                actionType: 'POST_CREATED',
+                post: newPost
+            });
         });
     },
     deletePost: (id) => {
